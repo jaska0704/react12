de02d7b72a319e1fd03b3432ce7eace8bdc9b00d
Jasurbek <jaskasamadov695@gmail.com>
2024-02-23 16:16:54 +0500

    121ws

--- src/pages/elektron/electron.jsx
+++ src/pages/elektron/electron.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { loadState } from '../../data/lib/storage';
 
 export const ElektronKitob = () => {
+  const token = loadState("user");
+
   return (
-  <div>
-    
-  </div>)
+    <div>
+      <img
+        className="w-[300px] h-[300px] rounded-full bg-slate-500 flex justify-center items-center relative"
+        src={token.user.foto}
+        alt=""
+      />
+    </div>
+  );
 };
